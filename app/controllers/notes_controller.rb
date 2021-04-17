72de10b340d2926d9f6c384236cab2e89d30d74a
mike <mikesapienzaaudio@gmail.com>
2021-04-17 14:17:38 -0500

    notes grid added, destroy functional

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,25 @@
 class NotesController < ApplicationController
+
+def new
+
+end
+
+def create
+
+end
+
+def edit
+  @note = Note.find(params[:id])
+  redirect_to_edit_note_path
+end
+
+def update
+
+end
+
+def destroy
+  Note.destroy(params[:id])
+  redirect_to root_path
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@ mike =  User.create(username:"mike", password: "123", password_confirmation: "12
 note1 = Note.create(title: "1", content: "123", user: mike)
 note2 = Note.create(title: "2", content: "456", user: mike)
 note3 = Note.create(title: "3", content: "789", user: mike)
+note4 = Note.create(title: "4", content: "10", user: mike)
+note5 = Note.create(title: "5", content: "11", user: mike)
+note6 = Note.create(title: "6", content: "12", user: mike)
 
