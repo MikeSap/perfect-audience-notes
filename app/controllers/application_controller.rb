80facb023488f2b965e409ec8643708a6c80073a
mike <mikesapienzaaudio@gmail.com>
2021-04-19 14:02:27 -0500

    dark mode added need to change nav link to symbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,24 @@
 class ApplicationController < ActionController::Base
-    before_action :current_user
-    before_action :require_login
+  before_action :current_user, :require_login, :set_theme
 
-    def current_user
-      @current_user = (User.find_by(id: session[:user_id]) || User.new)      
+  def current_user
+    @current_user = (User.find_by(id: session[:user_id]) || User.new)      
+  end
+
+  def logged_in
+    current_user.id != nil
+  end
+
+  def require_login
+    return redirect_to login_path unless logged_in
+  end
+
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      cookies[:theme] = theme
+      redirect_to(request.referrer || root_path)
     end
-  
-    def logged_in
-      current_user.id != nil
-    end
-  
-    def require_login
-      return redirect_to login_path unless logged_in
-    end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
 
   skip_before_action :require_login, only: [:new, :create]
 
-  def index
-  end
-
   def new        
     @user = User.new
   end
